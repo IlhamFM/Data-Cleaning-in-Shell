09eea1731b22d88c757b9dc436acf23d2e24cbec
IlhamFM <ilhamfaisalm@gmail.com>
2022-07-24 22:32:37 +0700

    revisi nama file csv

--- data_cleansing.sh
+++ data_cleansing.sh
@@ -5,5 +5,5 @@ csvgrep -c "event_type" -m "purchase" 2019-Oct-Nov-1.csv > 2019-Oct-Nov-2.csv
 csvcut -c 2,3,4,5,7,8 2019-Oct-Nov-2.csv > 2019-Oct-Nov-3.csv
 csvcut -c "category_code" 2019-Oct-Nov-2.csv | awk -F "." '{print $1}' > kategori-1.csv
 csvcut -c "category_code" 2019-Oct-Nov-2.csv | awk -F "." '{print $NF}' > kategori-2.csv
-csvjoin 2019-Oct-Nov-3.csv kategori-1.csv kategori-2.csv > 2019-Oct-Nov-Final.csv
-csvlook 2019-Oct-Nov-Final.csv | head -20
+csvjoin 2019-Oct-Nov-3.csv kategori-1.csv kategori-2.csv > data_clean.csv
+csvlook	data_clean.csv | head -20
